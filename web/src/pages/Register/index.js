5d853db238fa66680064574b17a5f7860c7c918d
André Arruda <andre23arruda@gmail.com>
2021-04-29 02:10:09 -0300

    add register page route

---
+++ web/src/pages/Register/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { FiLogIn } from 'react-icons/fi'
+import './styles.css'
+
+import logoImg from '../../assets/logo.svg'
+import heroesImg from '../../assets/heroes.png'
+
+function Register() {
+
+  	return (
+        <div className="logon-container">
+            Register
+        </div>
+  	)
+}
+
+export default Register
--- web/src/routes.js
+++ web/src/routes.js
@@ -1,12 +1,15 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import React from 'react'
 import Logon from './pages/Logon'
+import Register from './pages/Register'
+
 
 function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" component={ Logon } />
+                <Route path="/" exact component={ Logon } />
+                <Route path="/register" component={ Register } />
             </Switch>
         </BrowserRouter>
     )
