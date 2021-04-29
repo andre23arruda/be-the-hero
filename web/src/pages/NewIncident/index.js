3c328a92ec785e3f24142f7d75d0a352b59b7fb3
André Arruda <andre23arruda@gmail.com>
2021-04-29 03:17:17 -0300

    new incident

---
+++ web/src/pages/NewIncident/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { FiArrowLeft } from 'react-icons/fi'
+import './styles.css'
+
+import logoImg from '../../assets/logo.svg'
+import heroesImg from '../../assets/heroes.png'
+
+function NewIncident() {
+
+  	return (
+        <div className="newIncident-container">
+            <div className="content">
+                <section>
+                    <img src={ logoImg } alt="Be the Hero"/>
+
+                    <h1>Cadastro</h1>
+                    <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
+
+                    <Link to="/" className="back-link">
+                        <FiArrowLeft size={ 25 } color="#E02041"/>
+                        Já tenho cadastro
+                    </Link>
+                </section>
+
+
+                <form action="">
+                    <input placeholder="Nome da ONG"/>
+                    <input type="email" placeholder="Email"/>
+                    <input placeholder="WhatsApp"/>
+
+                    <div className="input-group">
+                        <input placeholder="Cidade"/>
+                        <input placeholder="UF"/>
+                    </div>
+
+                    <button className="button" type="submit">Cadastrar</button>
+
+                </form>
+            </div>
+
+        </div>
+  	)
+}
+
+export default NewIncident
--- web/src/routes.js
+++ web/src/routes.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Logon from './pages/Logon'
 import Register from './pages/Register'
 import Profile from './pages/Profile'
+import newIncident from './pages/NewIncident'
 
 
 function Routes() {
@@ -12,6 +13,7 @@ function Routes() {
                 <Route path="/" exact component={ Logon } />
                 <Route path="/register" component={ Register } />
                 <Route path="/profile" component={ Profile } />
+                <Route path="/new-incident" component={ newIncident } />
             </Switch>
         </BrowserRouter>
     )
