4abdad4c483e203d5d2808ea95cb4df5200ae83e
André Arruda <andre23arruda@gmail.com>
2021-04-29 02:37:30 -0300

    start profile page

---
+++ web/src/pages/Profile/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { FiArrowLeft } from 'react-icons/fi'
+import './styles.css'
+
+import logoImg from '../../assets/logo.svg'
+import heroesImg from '../../assets/heroes.png'
+
+function Profile() {
+
+  	return (
+        <div className="profile-container">
+            profile
+
+        </div>
+  	)
+}
+
+export default Profile
--- web/src/routes.js
+++ web/src/routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import React from 'react'
 import Logon from './pages/Logon'
 import Register from './pages/Register'
+import Profile from './pages/Profile'
 
 
 function Routes() {
@@ -10,6 +11,7 @@ function Routes() {
             <Switch>
                 <Route path="/" exact component={ Logon } />
                 <Route path="/register" component={ Register } />
+                <Route path="/profile" component={ Profile } />
             </Switch>
         </BrowserRouter>
     )
