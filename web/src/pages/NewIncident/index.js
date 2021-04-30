40e7292b04c631a01592e689c2981d634becf849
André Arruda <andre23arruda@gmail.com>
2021-04-30 14:15:23 -0300

    new incident page style

--- web/src/pages/NewIncident/index.js
+++ web/src/pages/NewIncident/index.js
@@ -14,26 +14,19 @@ function NewIncident() {
                 <section>
                     <img src={ logoImg } alt="Be the Hero"/>
 
-                    <h1>Cadastro</h1>
-                    <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
+                    <h1>Novo incidente</h1>
 
-                    <Link to="/" className="back-link">
+                    <Link to="/profile" className="back-link">
                         <FiArrowLeft size={ 25 } color="#E02041"/>
-                        Já tenho cadastro
+                        Voltar
                     </Link>
                 </section>
 
 
                 <form action="">
                     <input placeholder="Nome da ONG"/>
-                    <input type="email" placeholder="Email"/>
-                    <input placeholder="WhatsApp"/>
-
-                    <div className="input-group">
-                        <input placeholder="Cidade"/>
-                        <input placeholder="UF"/>
-                    </div>
-
+                    <textarea placeholder="Descrição"></textarea>
+                    <input placeholder="Valor (R$)"/>
                     <button className="button" type="submit">Cadastrar</button>
 
                 </form>
