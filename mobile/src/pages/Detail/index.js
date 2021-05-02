8eedaefa0de14388cbed720f5e8f5359da9b0dc7
André Arruda <andre23arruda@gmail.com>
2021-05-01 21:13:44 -0300

    add logo. Create pages

---
+++ mobile/src/pages/Detail/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import { View } from 'react-native'
+
+
+function Detail() {
+
+  	return (
+        <View></View>
+  	)
+}
+
+export default Detail
---
+++ mobile/src/pages/Incidents/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import { View } from 'react-native'
+
+
+function Incidents() {
+
+  	return (
+        <View></View>
+  	)
+}
+
+export default Incidents
